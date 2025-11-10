86b1b49f4752c6e626cf70fb06fe0982874e4aa6
Pat <arevalo.patpat08@gmail.com>
2025-11-10 23:01:50 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const open1 = document.getElementById("open1");
+const open2 = document.getElementById("open2");
+const open3 = document.getElementById("open3");
+const open4 = document.getElementById("open4");
+
+function modal(){
+    
+}
+
+document.getElementById("year").textContent = new Date().getFullYear();
